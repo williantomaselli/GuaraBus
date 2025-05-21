52cd42faaffb2c942142debdbfa023294042aead
willdev16 <williantomaselli@gmail.com>
2025-05-20 21:49:55 -0300

    adicionando rotas

--- public/js/motorista.js
+++ public/js/motorista.js
@@ -1,27 +1,58 @@
 let map;
 let rotaAtual = null;
+let pontosAtuais = [];
+let localizacaoUsuario = null;
 
 const rotas = {
-  "Guaramirim/Jaraguá via Centro": [
-    [-26.4687, -49.0021], // Ponto em Guaramirim
-    [-26.4800, -49.0500], // No caminho
-    [-26.4900, -49.0687]  // Centro Jaraguá
-  ],
-  "Jaraguá/Guaramirim via Centro": [
-    [-26.478641403865968, -49.085274426670075],
-    [-26.47971840341887, -49.08586753461779],
-    [-26.4687, -49.0021]
-  ],
-  "Guaramirim/Jaraguá via Corticeira": [
-    [-26.4687, -49.0021],
-    [-26.4700, -49.0300],
-    [-26.4900, -49.0687]
-  ],
-  "Jaraguá/Guaramirim via Corticeira": [
-    [-26.4900, -49.0687],
-    [-26.4700, -49.0300],
-    [-26.4687, -49.0021]
-  ]
+  "Guaramirim/Jaraguá via Centro": {
+    rota: [
+      [-26.4687, -49.0021],
+      [-26.4800, -49.0500],
+      [-26.4900, -49.0687]
+    ],
+    pontos: [
+      { nome: "Terminal Guaramirim", coords: [-26.4687, -49.0021] },
+      { nome: "Ponto 1", coords: [-26.4750, -49.0200] },
+      { nome: "Ponto 2", coords: [-26.4850, -49.0600] },
+      { nome: "Centro Jaraguá", coords: [-26.4900, -49.0687] }
+    ]
+  },
+  "Jaraguá/Guaramirim via Centro": {
+    rota: [
+      [-26.4786, -49.0852],
+      [-26.4797, -49.0858],
+      [-26.4687, -49.0021]
+    ],
+    pontos: [
+      { nome: "Centro Jaraguá", coords: [-26.4786, -49.0852] },
+      { nome: "Ponto 1", coords: [-26.4750, -49.0500] },
+      { nome: "Terminal Guaramirim", coords: [-26.4687, -49.0021] }
+    ]
+  },
+  "Guaramirim/Jaraguá via Corticeira": {
+    rota: [
+      [-26.4687, -49.0021],
+      [-26.4700, -49.0300],
+      [-26.4900, -49.0687]
+    ],
+    pontos: [
+      { nome: "Terminal Guaramirim", coords: [-26.4687, -49.0021] },
+      { nome: "Ponto Corticeira", coords: [-26.4700, -49.0300] },
+      { nome: "Centro Jaraguá", coords: [-26.4900, -49.0687] }
+    ]
+  },
+  "Jaraguá/Guaramirim via Corticeira": {
+    rota: [
+      [-26.4900, -49.0687],
+      [-26.4700, -49.0300],
+      [-26.4687, -49.0021]
+    ],
+    pontos: [
+      { nome: "Centro Jaraguá", coords: [-26.4900, -49.0687] },
+      { nome: "Ponto Corticeira", coords: [-26.4700, -49.0300] },
+      { nome: "Terminal Guaramirim", coords: [-26.4687, -49.0021] }
+    ]
+  }
 };
 
 function initMap() {
@@ -31,42 +62,81 @@ function initMap() {
     attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
 
+  pedirLocalizacao();
 
-  // Escuta mudança na rota selecionada
   const seletor = document.getElementById('rota');
   seletor.addEventListener('change', function () {
     const rotaSelecionada = this.value;
     if (rotas[rotaSelecionada]) {
-      desenharRota(rotas[rotaSelecionada]);
+      desenharRota(rotaSelecionada);
     }
   });
 }
 
-function desenharRota(pontos) {
-  // Remove rota anterior se houver
+function desenharRota(nomeRota) {
+  // Remove rota anterior
   if (rotaAtual) {
     map.removeLayer(rotaAtual);
   }
 
-  // Cria nova linha
-  rotaAtual = L.polyline(pontos, {
+  pontosAtuais.forEach(p => map.removeLayer(p));
+  pontosAtuais = [];
+
+  const rota = rotas[nomeRota].rota;
+  const pontos = rotas[nomeRota].pontos;
+
+  // Linha da rota
+  rotaAtual = L.polyline(rota, {
     color: '#00caf8',
     weight: 5
   }).addTo(map);
 
-  // Ajusta o zoom para caber a rota
   map.fitBounds(rotaAtual.getBounds());
 
-  // Adiciona marcadores nos pontos
-  pontos.forEach(ponto => {
-    L.circleMarker(ponto, {
-      radius: 6,
-      color: '#5015bd',
-      fillColor: '#027fe9',
-      fillOpacity: 0.6
+  // Apenas bolinhas nos extremos da rota
+  [0, rota.length - 1].forEach(i => {
+    const marker = L.circleMarker(rota[i], {
+      radius: 8,
+      color: '#0d47a1',
+      fillColor: '#42a5f5',
+      fillOpacity: 0.8
     }).addTo(map);
+    pontosAtuais.push(marker);
+  });
+
+  // Pontos de ônibus com nome
+  pontos.forEach(p => {
+    const marker = L.marker(p.coords)
+      .addTo(map)
+      .bindPopup(p.nome);
+    pontosAtuais.push(marker);
   });
 }
 
-// Inicializa quando carregar
+function pedirLocalizacao() {
+  if (!navigator.geolocation) {
+    alert("Geolocalização não suportada pelo navegador.");
+    return;
+  }
+
+  navigator.geolocation.watchPosition(pos => {
+    const { latitude, longitude } = pos.coords;
+
+    if (localizacaoUsuario) {
+      localizacaoUsuario.setLatLng([latitude, longitude]);
+    } else {
+      localizacaoUsuario = L.circleMarker([latitude, longitude], {
+        radius: 8,
+        color: '#2196f3',
+        fillColor: '#2196f3',
+        fillOpacity: 0.9
+      }).addTo(map).bindPopup("Você está aqui").openPopup();
+    }
+  }, () => {
+    alert("Não foi possível obter sua localização.");
+  }, {
+    enableHighAccuracy: true
+  });
+}
+
 window.onload = initMap;
