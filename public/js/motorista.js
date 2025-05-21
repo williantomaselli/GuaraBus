5c61b518a26f73613ba025b1f349fe004e8ae8b1
willdev16 <williantomaselli@gmail.com>
2025-05-20 21:33:01 -0300

    sistema de rotas adicionados

--- public/js/motorista.js
+++ public/js/motorista.js
@@ -1,21 +1,72 @@
+let map;
+let rotaAtual = null;
+
+const rotas = {
+  "Guaramirim/Jaraguá via Centro": [
+    [-26.4687, -49.0021], // Ponto em Guaramirim
+    [-26.4800, -49.0500], // No caminho
+    [-26.4900, -49.0687]  // Centro Jaraguá
+  ],
+  "Jaraguá/Guaramirim via Centro": [
+    [-26.478641403865968, -49.085274426670075],
+    [-26.47971840341887, -49.08586753461779],
+    [-26.4687, -49.0021]
+  ],
+  "Guaramirim/Jaraguá via Corticeira": [
+    [-26.4687, -49.0021],
+    [-26.4700, -49.0300],
+    [-26.4900, -49.0687]
+  ],
+  "Jaraguá/Guaramirim via Corticeira": [
+    [-26.4900, -49.0687],
+    [-26.4700, -49.0300],
+    [-26.4687, -49.0021]
+  ]
+};
+
 function initMap() {
-    
-    // cria o mapa centralizado
-    const map = L.map('map').setView([-26.4900, -49.0687], 13);
-  
-    // tiles do OpenStreetMap
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution:
-        '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-  
-    // exemplo de marcador
-    L.marker([-26.4900, -49.0687])
-      .addTo(map)
-      .bindPopup('Centro de Jaraguá do Sul')
-      .openPopup();
+  map = L.map('map').setView([-26.4900, -49.0687], 13);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+
+  // Escuta mudança na rota selecionada
+  const seletor = document.getElementById('rota');
+  seletor.addEventListener('change', function () {
+    const rotaSelecionada = this.value;
+    if (rotas[rotaSelecionada]) {
+      desenharRota(rotas[rotaSelecionada]);
+    }
+  });
+}
+
+function desenharRota(pontos) {
+  // Remove rota anterior se houver
+  if (rotaAtual) {
+    map.removeLayer(rotaAtual);
   }
-  
-  // chama initMap assim que o JS for carregado
-  window.onload = initMap;
-  
+
+  // Cria nova linha
+  rotaAtual = L.polyline(pontos, {
+    color: '#00caf8',
+    weight: 5
+  }).addTo(map);
+
+  // Ajusta o zoom para caber a rota
+  map.fitBounds(rotaAtual.getBounds());
+
+  // Adiciona marcadores nos pontos
+  pontos.forEach(ponto => {
+    L.circleMarker(ponto, {
+      radius: 6,
+      color: '#5015bd',
+      fillColor: '#027fe9',
+      fillOpacity: 0.6
+    }).addTo(map);
+  });
+}
+
+// Inicializa quando carregar
+window.onload = initMap;
